8356c1cacbb2a402f6b0fcdd7d889413710d18ca
szpadelinho <f.goralski2@outlook.com>
2024-04-13 17:24:39 +0200

    Add another skeleton sequence

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import { useState } from 'react'
-import Header from "./Header.jsx";
+import Header from "./Header.jsx"
 import Body from "./Body.jsx"
 import Footer from "./Footer.jsx"
+import Search from "./Search.jsx"
 
 function App() {
+
+  const [isVisibleBody, setVisibleBody] = useState(false)
+  const [isVisibleSearch, setVisibleSearch] = useState(true)
 
   return (
     <main>
       <Header/>
-      <Body/>
+      {isVisibleBody && <Body/>}
+      {isVisibleSearch && <Search/>}
       <Footer/>
     </main>
   )
---
+++ src/Search.jsx
@@ -0,0 +1,52 @@
+const Search = () => {
+    return(
+        <div className="Search">
+            <div className="Search_Content">
+                <ul>
+                    <li>
+                        <div id="list_cover">
+                            <img src="https://cdns-images.dzcdn.net/images/cover/412361ce41f0bd2595978dbf0e035ad3/500x500.jpg"/>
+                        </div>
+                        <div id="list_info">
+                            <p>Mr. Morale & The Big Steppers</p>
+                            <p>Kendrick Lamar</p>
+                            <p>2022</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="list_cover">
+                            <img src="https://cdns-images.dzcdn.net/images/cover/412361ce41f0bd2595978dbf0e035ad3/500x500.jpg"/>
+                        </div>
+                        <div id="list_info">
+                            <p>Mr. Morale & The Big Steppers</p>
+                            <p>Kendrick Lamar</p>
+                            <p>2022</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="list_cover">
+                            <img src="https://cdns-images.dzcdn.net/images/cover/412361ce41f0bd2595978dbf0e035ad3/500x500.jpg"/>
+                        </div>
+                        <div id="list_info">
+                            <p>Mr. Morale & The Big Steppers</p>
+                            <p>Kendrick Lamar</p>
+                            <p>2022</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="list_cover">
+                            <img src="https://cdns-images.dzcdn.net/images/cover/412361ce41f0bd2595978dbf0e035ad3/500x500.jpg"/>
+                        </div>
+                        <div id="list_info">
+                            <p>Mr. Morale & The Big Steppers</p>
+                            <p>Kendrick Lamar</p>
+                            <p>2022</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Search
